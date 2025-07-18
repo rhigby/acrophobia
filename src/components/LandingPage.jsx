044a6eca2da83e9ea550da2949ef64aebe0d32a3
rhigby <ralphhigby@gmail.com>
2025-07-18 18:57:01 -0400

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -141,7 +141,7 @@ export default function LandingPage() {
   };
 
   return (
-    <div className="min-h-screen text-white font-sans" >
+    <div className="min-h-screen text-white font-sans bg-gradient-to-br from-black via-blue-900 to-black">
       <header className="sticky top-0 z-50 bg-black border-b border-blue-800 shadow-md py-4 px-6 flex justify-between items-center">
         <h1 className="text-3xl font-bold text-red-600 drop-shadow-[0_0_6px_orange]">Acrophobia</h1>
       </header>
